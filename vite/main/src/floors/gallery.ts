8c726efc27753d5cfdc9a468b923d888d8ce3a4b
NorthWestWind <wsyn148@gmail.com>
2025-02-20 09:39:26 +0800

    correctly first load gallery

--- vite/main/src/floors/gallery.ts
+++ vite/main/src/floors/gallery.ts
@@ -8,13 +8,12 @@ import { LazyLoader } from "../types/misc";
 
 const TEMPLATE = new LazyLoader(() => fetchText("/contents/gallery/template.html"));
 
-const PAINTINGS = 23;
 const PAINTING_LENGTH = 50;
 const LENGTH_PER_PAINTING = PAINTING_LENGTH + 30;
-const FLOOR_LENGTH = Math.ceil((PAINTINGS - 1) * 0.5) * LENGTH_PER_PAINTING;
 
 export default class GalleryFloor extends Floor {
-	private static fileNames: string[];
+	private static fileNames: Promise<string[]>;
+	private static floorLength: number;
 	paintings: THREE.Mesh[] = [];
 
 	constructor() {
@@ -24,61 +23,62 @@ export default class GalleryFloor extends Floor {
 	}
 
 	static {
-		fetch(`/api/config`).then(async res => {
-			if (!res.ok) return;
-			const files = <string[]>(await res.json()).pfps;
-			this.fileNames = files.sort();
-		});
+		this.fileNames = fetch(`/api/config`).then(res => res.json()).then(json => json.pfps.sort());
+		this.floorLength = 0;
+		this.fileNames.then(pfps => this.floorLength = Math.ceil((pfps.length - 1) * 0.5) * LENGTH_PER_PAINTING);
 	}
 
-	spawn(scene: THREE.Scene) {
+	async spawn(scene: THREE.Scene) {
+		const fileNames = await GalleryFloor.fileNames;
+		const paintings = fileNames.length;
+		const floorLength = Math.ceil((paintings - 1) * 0.5) * LENGTH_PER_PAINTING;
 		const objects: any = {};
 
-		const geometryR = new THREE.BoxGeometry(50, 2, FLOOR_LENGTH - 40);
+		const geometryR = new THREE.BoxGeometry(50, 2, floorLength - 40);
 		const materialR = new THREE.MeshStandardMaterial({ color: 0xad0000 });
 		const rug = new THREE.Mesh(geometryR, materialR);
-		rug.position.set(0, this.num * 1000 - 32, -(FLOOR_LENGTH - 40) * 0.5 - 50);
+		rug.position.set(0, this.num * 1000 - 32, -(floorLength - 40) * 0.5 - 50);
 		scene.add(rug);
 		objects.rug = rug;
 
-		const geometryF = new THREE.BoxGeometry(80, 2, FLOOR_LENGTH);
+		const geometryF = new THREE.BoxGeometry(80, 2, floorLength);
 		const materialF = new THREE.MeshStandardMaterial({ color: 0xfef0bc });
 		const floor = new THREE.Mesh(geometryF, materialF);
-		floor.position.set(0, this.num * 1000 - 33, -FLOOR_LENGTH * 0.5 - 50);
+		floor.position.set(0, this.num * 1000 - 33, -floorLength * 0.5 - 50);
 		scene.add(floor);
 		objects.floor = floor;
 
-		for (let ii = 1; ii < Math.floor(FLOOR_LENGTH / LENGTH_PER_PAINTING); ii++) {
+		for (let ii = 1; ii < Math.floor(floorLength / LENGTH_PER_PAINTING); ii++) {
 			const pointLight = new THREE.PointLight(0xffffff, 92, 90, 0.9);
 			pointLight.position.set(0, this.num * 1000, -LENGTH_PER_PAINTING * (ii + 0.5) - 30);
 			scene.add(pointLight);
 			objects["light"+ii] = pointLight;
 		}
 
-		const geometryS = new THREE.BoxGeometry(3, 80, FLOOR_LENGTH);
+		const geometryS = new THREE.BoxGeometry(3, 80, floorLength);
 		const wallL = new THREE.Mesh(geometryS, materialF);
 		const wallR = new THREE.Mesh(geometryS, materialF);
-		wallL.position.set(-40, this.num * 1000 - 5, -FLOOR_LENGTH * 0.5 - 50);
-		wallR.position.set(40, this.num * 1000 - 5, -FLOOR_LENGTH * 0.5 - 50);
+		wallL.position.set(-40, this.num * 1000 - 5, -floorLength * 0.5 - 50);
+		wallR.position.set(40, this.num * 1000 - 5, -floorLength * 0.5 - 50);
 		scene.add(wallL, wallR);
 		objects.wallL = wallL;
 		objects.wallR = wallR;
 
-		const geometryC = new THREE.BoxGeometry(80, 2, FLOOR_LENGTH);
+		const geometryC = new THREE.BoxGeometry(80, 2, floorLength);
 		const ceiling = new THREE.Mesh(geometryC, materialF);
-		ceiling.position.set(0, this.num * 1000 + 36, -FLOOR_LENGTH * 0.5 - 50);
+		ceiling.position.set(0, this.num * 1000 + 36, -floorLength * 0.5 - 50);
 		scene.add(ceiling);
 		objects.ceiling = ceiling;
 
 		const geometryB = new THREE.BoxGeometry(80, 80, 2);
 		const back = new THREE.Mesh(geometryB, materialF);
-		back.position.set(0, this.num * 1000 - 5, -FLOOR_LENGTH - 50);
+		back.position.set(0, this.num * 1000 - 5, -floorLength - 50);
 		scene.add(back);
 		objects.back = back;
 
-		for (let ii = 0; ii < PAINTINGS - 1; ii++) {
+		for (let ii = 0; ii < paintings - 1; ii++) {
 			const geometry = new THREE.BoxGeometry(2, 50, 50);
-			const texture = TEXTURE_LOADER.load(`/assets/pfps/smaller/${GalleryFloor.fileNames[ii]}`, texture => {
+			const texture = TEXTURE_LOADER.load(`/assets/pfps/smaller/${fileNames[ii]}`, texture => {
 				texture.generateMipmaps = false;
 				texture.magFilter = THREE.NearestFilter;
 				texture.minFilter = THREE.LinearMipMapLinearFilter;
@@ -100,7 +100,7 @@ export default class GalleryFloor extends Floor {
 		}
 
 		const geometryP = new THREE.BoxGeometry(50, 50, 2);
-		const texture = TEXTURE_LOADER.load(`/assets/pfps/smaller/${GalleryFloor.fileNames[PAINTINGS - 1]}`, texture => {
+		const texture = TEXTURE_LOADER.load(`/assets/pfps/smaller/${fileNames[paintings - 1]}`, texture => {
 			texture.generateMipmaps = false;
 			texture.magFilter = THREE.NearestFilter;
 			texture.minFilter = THREE.LinearMipMapLinearFilter;
@@ -109,7 +109,7 @@ export default class GalleryFloor extends Floor {
 		const xm = new THREE.MeshBasicMaterial({ map: texture });
 		const materialP = new THREE.MeshStandardMaterial({ color: 0xffffff });
 		const painting = new THREE.Mesh(geometryP, [materialP, materialP, materialP, materialP, xm, xm]);
-		painting.position.set(0, this.num * 1000 + 5, -FLOOR_LENGTH - 49);
+		painting.position.set(0, this.num * 1000 + 5, -floorLength - 49);
 		scene.add(painting);
 		this.paintings.push(painting);
 		objects.painting = painting;
@@ -120,7 +120,7 @@ export default class GalleryFloor extends Floor {
 	handleWheel(scroll: number) {
 		let maxed = false;
 		const cam = camera();
-		const maxDist = FLOOR_LENGTH + 10;
+		const maxDist = GalleryFloor.floorLength + 10;
 		if (cam.position.z >= -maxDist) cam.translateZ(-scroll);
 		if (cam.position.z < -maxDist) {
 			cam.position.setZ(-maxDist);
@@ -137,7 +137,7 @@ export default class GalleryFloor extends Floor {
 
 	private openOrCloseGalleryInfo(index: number) {
 		toggleContent({ html: async () => {
-			const file = GalleryFloor.fileNames[index];
+			const file = (await GalleryFloor.fileNames)[index];
 			if (!file) return "";
 			return (await TEMPLATE.get()).replace("{title}", file.split(" ").slice(1).join(" ").split(".").slice(0, -1).join(".")).replace("{src}", `assets/pfps/${file}`);
 		}})
