d23af641780d77eea3ff09beef2fdbacf0fccb28
North-West-Wind <wsyn148@gmail.com>
2025-03-20 12:27:05 +0800

    a little bit more lore

--- vite/summatia-editor/src/app.tsx
+++ vite/summatia-editor/src/app.tsx
@@ -20,13 +20,13 @@ export function App() {
 	const download = () => {
 		if (!data) return;
 		const element = document.createElement('a');
-		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(data)));
+		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(data.toData())));
 		element.setAttribute('download', "summatia.json");
 		element.style.display = 'none';
 		document.body.appendChild(element);
 		element.click();
 		document.body.removeChild(element);
-		window.localStorage.setItem("summatiaData", JSON.stringify(data));
+		window.localStorage.setItem("summatiaData", JSON.stringify(data.toData()));
 	};
 
   useEffect(() => {
